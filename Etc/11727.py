fefd69c8b11361063dfd1df8e9618653ef4d815d
anismynm <ddoe0318@naver.com>
2024-11-15 00:03:22 +0900

    Update 11727.py

--- Etc/11727.py
+++ Etc/11727.py
@@ -3,3 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 
+dp = [0 for _ in range(n)]
+
+for i in range(n):
+    if i == 0:
+        dp[i] = 1
+    elif i == 1:
+        dp[i] = 3
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2] * 2
+
+print(dp[-1] % 10007)
